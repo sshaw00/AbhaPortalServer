f009ef8e5e2cacc83723ddc97bd54f20d487edb8
sshaw00 <swarupk102001@gmail.com>
2024-03-06 13:56:21 +0530

    new changes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,14 +8,14 @@ const {
   logout,
 } = require("../controllers/auth");
 const { registerValidation, loginValidation } = require("../validators/auth");
-const {
-  validationMiddleware,
-} = require("../middlewares/validations-middleware.js");
+// const {
+//   validationMiddleware,
+// } = require("../middlewares/validations-middleware.js");
 const { userAuth } = require("../middlewares/auth-middleware.js");
 
 router.get("/get-users", getUsers);
 router.get("/protected", userAuth, protected);
-router.post("/register", registerValidation, validationMiddleware, register);
+router.post("/register", registerValidation, register);
 router.post("/login", loginValidation, login);
 router.get("/logout", logout);
 module.exports = router;
