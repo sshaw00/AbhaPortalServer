bb0571a17009a5d7d101c99953bba25863e3d3be
sshaw00 <swarupk102001@gmail.com>
2024-03-07 14:47:54 +0530

    validations

--- src/middlewares/validations-middleware.js
+++ src/middlewares/validations-middleware.js
@@ -1,13 +1,13 @@
-// const { validationResult } = require("express-validator");
+const { validationResult } = require("express-validator");
 
-// exports.validationMiddleware = (req, res, next) => {
-//   let errors = validationResult(req);
+exports.validationMiddleware = (req, res, next) => {
+  let errors = validationResult(req);
 
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({
-//       errors: errors.array(),
-//     });
-//   }
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
 
-//   next();
-// };
+  next();
+};
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,15 +9,15 @@ const {
   random,
 } = require("../controllers/auth");
 const { registerValidation, loginValidation } = require("../validators/auth");
-// const {
-//   validationMiddleware,
-// } = require("../middlewares/validations-middleware.js");
+const {
+  validationMiddleware,
+} = require("../middlewares/validations-middleware.js");
 const { userAuth } = require("../middlewares/auth-middleware.js");
 
 router.get("/get-users", getUsers);
 router.get("/protected", userAuth, protected);
-router.post("/register", registerValidation, register);
-router.post("/login", loginValidation, login);
+router.post("/register", registerValidation, validationMiddleware, register);
+router.post("/login", loginValidation, validationMiddleware, login);
 router.get("/logout", logout);
 router.get("/random", random);
 module.exports = router;
