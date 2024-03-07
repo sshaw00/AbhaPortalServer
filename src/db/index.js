40c1c7938f5e980a7861e0c19491ec82ce6f27a3
sshaw00 <swarupk102001@gmail.com>
2024-03-07 14:18:22 +0530

    ssl added

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,7 @@ const pool = new Pool({
   database: process.env.PSQL_DATABASE,
   password: process.env.PSQL_PASSWORD,
   port: process.env.PSQL_PORT,
+  ssl: true,
 });
 module.exports = {
   query: (text, params) => pool.query(text, params),
